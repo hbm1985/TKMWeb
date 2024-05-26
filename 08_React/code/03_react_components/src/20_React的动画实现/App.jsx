627d555506a6c2b8af21ec160de7cbb2f0013c8f
刘德龙 <793854601@qq.com>
2024-05-27 01:51:12 +0800

    React过渡动画其他属性

--- 08_React/code/03_react_components/src/20_React的动画实现/App.jsx
+++ 08_React/code/03_react_components/src/20_React的动画实现/App.jsx
@@ -43,6 +43,34 @@
     第一类，开始状态：对应的类是-enter、-exit
     第二类，执行动画：对应的类是enter-active、-exit-active
     第三类，执行结束：对应的类是-enter-done、-exit-done
+
+  CSSTransition常见的属性
+
+  in：触发进入或者退出状态
+    如果添加了unmountOnExit={true}，那么该组件会在执行退出动画结束后被移除掉
+    当in为true时，触发进入状态，会添加-enter、-enter-active的class开始执行动画，当动画执行结束，会移除两个class，并添加-enter-done的class
+    当in为false时，触发退出状态，会添加-exit、-exit-active的class开始执行动画，当动画执行结束，会移除两个class，并添加-exit-done的class
+
+  classNames：动画class的名称
+    决定了在编写css时，对应的class名称：-enter、-enter-active、-enter-done、-exit、-exit-active、-exit-done
+
+  timeout：过渡动画的时间
+    一定要与css的transition时间保持一致
+    timeout时间到了，添加对应的类（-enter-done、-exit-done）
+
+  appear：是否在初次进入添加动画（需要和in同时为true）
+
+  unmountOnExit：退出后是否卸载组件
+
+  其他属性：https://reactcommunity.org/react-transition-group/transition
+    
+  CSStransition对应的钩子函数：主要为了检测动画的执行过程，来完成一些JavaScript的操作
+    onEnter：进入动画执行之前被触发
+    onEntering：进入动画执行时被触发
+    onEntered：进入动画执行结束被触发
+    onExit：离开动画执行之前被触发
+    onExiting：离开动画执行时被触发
+    onExited：离开动画执行结束被触发
  */
 
 import React, { PureComponent } from "react"
@@ -53,7 +81,7 @@ export default class App extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      showTitle: false
+      showTitle: true
     }
   }
 
@@ -63,7 +91,19 @@ export default class App extends PureComponent {
       <div>
         <button onClick={() => this.setState({ showTitle: !showTitle })}>切换显示/隐藏</button>
         {/* {showTitle && <h2>哈哈哈</h2>} */}
-        <CSSTransition in={showTitle} classNames="bread" timeout={1000} unmountOnExit={true}>
+        <CSSTransition
+          appear
+          in={showTitle}
+          classNames="bread"
+          timeout={1000}
+          unmountOnExit={true}
+          onEnter={() => console.log("开始进入动画")}
+          onEntering={() => console.log("执行进入动画")}
+          onEntered={() => console.log("进入动画结束")}
+          onExit={() => console.log("开始离开动画")}
+          onExiting={() => console.log("执行离开动画")}
+          onExited={() => console.log("离开动画结束")}
+        >
           <div>
             <h2>哈哈哈</h2>
             <div>嘿嘿嘿</div>
