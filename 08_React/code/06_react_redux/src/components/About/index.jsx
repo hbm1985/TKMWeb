5f741a37d12ebc3b1a40d2e795b48a9f5ae15e7a
刘德龙 <793854601@qq.com>
2024-05-28 13:22:20 +0800

    react-redux_映射dispatch

--- 08_React/code/06_react_redux/src/components/About/index.jsx
+++ 08_React/code/06_react_redux/src/components/About/index.jsx
@@ -1,5 +1,6 @@
 import React, { PureComponent } from "react"
 import { connect } from "react-redux"
+import { createIncrementAction, createDecrermentAction } from "../../store/actionCreators"
 
 //  定义UI组件
 class About extends PureComponent {
@@ -7,22 +8,48 @@ class About extends PureComponent {
     super(props)
   }
 
+  calcNumber(number, isAdd) {
+    if (isAdd) {
+      const { increment } = this.props
+      increment(number)
+    } else {
+      const { decrement } = this.props
+      decrement(number)
+    }
+  }
+
   render() {
     const { count } = this.props
     return (
       <div>
         <h2>About counter: {count}</h2>
+        <button onClick={() => this.calcNumber(1, true)}>+1</button>
+        <button onClick={() => this.calcNumber(5, true)}>+5</button>
+        <button onClick={() => this.calcNumber(8, true)}>+8</button>
+        <button onClick={() => this.calcNumber(1, false)}>-1</button>
+        <button onClick={() => this.calcNumber(5, false)}>-5</button>
+        <button onClick={() => this.calcNumber(8, false)}>-8</button>
       </div>
     )
   }
 }
 
 //  将state映射到props
-function mapStateToProps(state) {
-  return {
-    count: state.count
-  }
-}
+const mapStateToProps = (state) => ({
+  count: state.count
+})
 
-//  connect()调用的返回值是高阶组件
-export default connect(mapStateToProps)(About)
+//  将dispatch映射到props
+const mapDispatchToProps = (dispatch) => ({
+  increment: (number) => {
+    dispatch(createIncrementAction(number))
+  },
+  decrement: (number) => {
+    dispatch(createDecrermentAction(number))
+  }
+})
+
+//  connect(mapStateToProps, mapDispatchToProps)调用的返回值是高阶组件
+//  第一个参数：映射的state
+//  第二个参数：映射的dispatch
+export default connect(mapStateToProps, mapDispatchToProps)(About)
--- 08_React/code/06_react_redux/src/index.js
+++ 08_React/code/06_react_redux/src/index.js
@@ -1,3 +1,17 @@
+/*
+  react-redux使用
+
+  开始之前需要强调一下，Redux和React没有直接的关系，你完全可以在React、Angular、vanilla JavaScript中使用Redux
+
+  尽管这样说，Redux依然是和React库结合的更好，因为他们是通过state函数来描述界面的状态，Redux可以发射状态的更新，让他们做出回应
+
+  虽然我们之前已经实现了connect、Provider这些帮助我们完成连接Redux、React的辅助工具，但是实际上redux官方帮助我们提供了react-redux的库
+
+  安装react-redux：
+    npm install react-redux
+    yarn add react-redux
+ */
+
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
