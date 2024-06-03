628218e3324b3b547b0f9cc37e2a8fc9eae06b65
刘德龙 <793854601@qq.com>
2024-06-03 14:03:33 +0800

    useState的使用详解和使用规则

--- 08_React/code/09_react_hooks/src/02_计数器实现对比/App.jsx
+++ 08_React/code/09_react_hooks/src/02_计数器实现对比/App.jsx
@@ -1,3 +1,28 @@
+/*
+  useState解析
+
+  那么我们来研究一下核心的一段代码表示什么意思：
+    useState来自react，需要从react中导入，它是一个hook
+    参数：初始化值，如果不设置，则为undefined
+    返回值：数组，包含两个元素
+      元素一：当前状态的值（第一次为初始化值）
+      元素二：设置状态值的函数
+
+    点击button后，会完成两个事情：
+      调用setCounter，设置一个新的值
+      组件重新渲染，并且根据最新的state值返回DOM结构
+
+  Hook就是JavaScript函数，这个函数可以帮助你钩入（hook into）React State以及生命周期等特性
+
+  但是使用它们会有两个额外的规则：
+    只能在函数最外层调用Hook，不要在循环、条件判断或者子函数中调用
+    只能在React的函数组件中调用Hook，不要在其他JavaScript函数中调用
+
+  Tips：
+    Hook指的是类似于useState、useEffect这样的函数
+    Hooks是对这类函数的统称
+ */
+
 import React, { memo } from "react"
 import CounterClass from "./CounterClass"
 import CounterFunc from "./CounterFunc"
--- 08_React/code/09_react_hooks/src/02_计数器实现对比/CounterFunc.jsx
+++ 08_React/code/09_react_hooks/src/02_计数器实现对比/CounterFunc.jsx
@@ -1,17 +1,32 @@
 import { memo, useState } from "react"
 
+//  普通的函数，里面不能使用Hooks
+//  在自定义的Hooks中，可以使用React提供的其他Hooks
+//  区分普通函数与自定义Hooks：是否已use开头
+function useFoo() {
+  const [message, setMessage] = useState("Hello")
+  return { message, setMessage }
+}
+
 //  函数组件实现计数器
 function CounterFunc(props) {
   console.log("CounterFunc")
-  
+
   //  useState：定义状态、修改状态的函数
   //  传入的参数作为状态的初始值
   //  当函数组件重新被渲染（执行）时，使用useState定义的状态不会被重置
   const [counter, setCounter] = useState(0)
 
+  //  React Hooks必须在函数组件内的顶级作用域使用
+  // if (true) {
+  //   //  React Hooks must be called in the exact same order in every component render
+  //   const [name, setName] = useState("Bread")
+  // }
+
   return (
     <div>
       <h2>当前计数：{counter}</h2>
+      {/* 每次调用setCounter之后，函数组件将会以最新的state值，重新渲染 */}
       <button onClick={() => setCounter(counter + 1)}>+1</button>
       <button onClick={() => setCounter(counter - 1)}>-1</button>
     </div>
