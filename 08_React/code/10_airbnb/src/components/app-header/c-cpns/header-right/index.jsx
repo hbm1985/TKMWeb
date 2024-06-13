0e09e15748501396bba4330aca44449056f7a22f
刘德龙 <793854601@qq.com>
2024-06-13 12:28:14 +0800

    集成styled-components、app-header结构封装

---
+++ 08_React/code/10_airbnb/src/components/app-header/c-cpns/header-right/index.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from "react"
+import { RightWrapper } from "./style"
+
+const HeaderRight = memo(() => {
+  return (
+    <RightWrapper>HeaderRightHeaderRight</RightWrapper>
+  )
+})
+
+export default HeaderRight
